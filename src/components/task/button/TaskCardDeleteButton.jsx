fd252cfb4c76c83632629825004f9c1c5214dddc
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-19 16:29:46 +0900

    taskCardDeleteButtonメソッドに引数を渡してfilterで合致しないものだけを抽出

--- src/components/task/button/TaskCardDeleteButton.jsx
+++ src/components/task/button/TaskCardDeleteButton.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
 export const TaskCardDeleteButton = ({ taskCardsList, setTaskCardsList, taskCard }) => {
-  const taskCardDeleteButton = () => {
-
+  const taskCardDeleteButton = (id) => {
+    //タスクカードを削除
+    setTaskCardsList(taskCardsList.filter((e) => e.id !== id));
   };
 
   return (
     <div>
       <button className='taskCardDeleteButton'>
-        <i className='fas fa-times' onClick={() => taskCardDeleteButton()}></i>
+        <i className='fas fa-times' onClick={() => taskCardDeleteButton(taskCard.id)}></i>
       </button>
     </div>
   )
