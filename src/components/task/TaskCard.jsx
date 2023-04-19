7442246dc4c0a9a034fd881b09782e3063bc5545
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-19 13:37:38 +0900

    コンポーネントを作成してTaskCardへimport

--- src/components/task/TaskCard.jsx
+++ src/components/task/TaskCard.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
+import { TaskCardDeleteButton } from './button/TaskCardDeleteButton'
+import { TaskAddInput } from './input/TaskAddInput'
+import { TaskCardTitle } from './TaskCardTitle'
+import { Tasks } from './Tasks'
+
 function TaskCard() {
   return (
-    <div>TaskCard</div>
+    <div>
+      <TaskCardTitle />
+      <TaskCardDeleteButton />
+      <TaskAddInput />
+      <Tasks />
+    </div>
   )
 }
 
