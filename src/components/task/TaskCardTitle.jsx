1c2f53d85565872876770afbf5b94c752e43344c
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-19 13:50:54 +0900

    inputに入力された内容を元にinputCardTitle変数を更新するhandleChangeメソッドを定義し、onChangeイベントに設定

--- src/components/task/TaskCardTitle.jsx
+++ src/components/task/TaskCardTitle.jsx
@@ -2,16 +2,21 @@ import React, { useState } from 'react'
 
 export const TaskCardTitle = () => {
   const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState("Today");
 
   const handleClick = () => {
     setIsClick(true)
+  };
+
+  const handleChange = (e) => {
+    setInputCardTitle(e.target.value);
   };
 
   return (
     <div onClick={handleClick}>
       {isClick ? (
         <form>
-          <input type="text" />
+          <input type="text" onChange={handleChange} />
         </form>) : (
         <h3>Today</h3>
         )
