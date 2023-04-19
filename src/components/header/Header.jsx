5b5cc7abc194212b142e3089fa71ad62806a080a
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-19 13:22:07 +0900

    index.cssにてレイアウト調整

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <header>
+        <h1>Simple Trello</h1>
+      </header>
+    </div>
   )
 }
