ad8362b409331beda7c58176467a19d4f5e9d3e1
issei-yoshi <iyoshi.engineer@gmail.com>
2023-04-19 15:25:08 +0900

    Tasksコンポーネントにて並び替えを行うhandleDragEndメソッドを定義

--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -3,10 +3,22 @@ import { DragDropContext, Droppable } from 'react-beautiful-dnd'
 
 import { Task } from './Task'
 
+const reorder = (taskList, startIndex, endIndex) => {
+  //taskを並び替える
+  const remove = taskList.splice(startIndex, 1);
+  taskList.splice(endIndex, 0, remove[0]);
+}
+
 export const Tasks = ({ taskList, setTaskList }) => {
+  const handleDragEnd = (result) => {
+    reorder(taskList, result.source.index, result.destination.index);
+
+    setTaskList(taskList);
+  }
+
   return (
     <div>
-      <DragDropContext>
+      <DragDropContext onDragEnd={handleDragEnd}>
         <Droppable droppableId='droppable'>
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
